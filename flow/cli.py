776105d7329a36c3b1f991f6675bb73cf5628eea
Lev <levwint@gmail.com>
2021-07-22 11:54:01 +0300

    ref(add-channel): Improve messages, make code easier to understand

--- flow/cli.py
+++ flow/cli.py
@@ -97,50 +97,60 @@ def run_command(channel: str):
 
 
 @cli.command("add-channel")
-def add_channel_command():  # TODO: Refactor
+def add_channel_command():
 
     if not click.confirm(
         f"""To add new channel you need to:
-    1. Add your bot @{conf.tg_bot_username} to Telegram channel 
+    1. Add your bot (@{conf.tg_bot_username}) to Telegram channel 
        in which you're planning to repost posts as Administrator
 
-    2. Send random message in this channel.
+    2. Send any message in this channel.
 
-If you have already done that, than hit 'Enter'.""",
+If you've done that, than hit 'Enter'.""",
         default=True,
     ):
         return
+
     vk_group_id: Optional[int] = None
     for i in range(5):  # type: ignore
         group_url = click.prompt(
             "Enter link to source VK group, for example, 'https://vk.com/vk'"
         )
         screen_name = re.findall(r"vk.com/([^/]+)", group_url)
-        if screen_name:
+
+        if not screen_name:
+            click.echo("Not valid VK Group URL. Try again.")
+        else:
             screen_name = screen_name[0]
             try:
                 group: dict[str, Any] = get_vk().groups.getById(
                     group_id=str(screen_name)
                 )[0]
-                click.echo(f'Got group "{group["name"]}"')
-                vk_group_id = int(group["id"])
-                if not str(vk_group_id).startswith("-"):
-                    vk_group_id = int(f"-{vk_group_id}")
-                break
             except vk_api.exceptions.ApiError as e:
                 if e.code == 100:
-                    click.echo(f"Group with screen name '{screen_name}' does not exist")
-        else:
-            click.echo("Not valid VK Group URL. Try again.")
+                    click.echo(
+                        f"Group with screen name '{screen_name}'"
+                        " does not exist. Try again."
+                    )
+                    continue
+                raise e
 
+            click.echo(f'Got group "{group["name"]}"')
+            vk_group_id = int(group["id"])
+            if not str(vk_group_id).startswith("-"):
+                vk_group_id = int(f"-{vk_group_id}")
+            break
     if not vk_group_id:
         return
 
     tg_chat_id = None
     for i in range(5):  # type: ignore
-        channel_name = click.prompt("""Enter target Telegram channel name, for example, 'Telegram News'""", type=str)  # type: ignore
-        bot = telegram.Bot(conf.tg_bot_token)
-        updates = bot.get_updates()
+        channel_name = click.prompt(
+            "Enter target Telegram channel name, for example, 'Telegram News'",
+            type=str,  # type: ignore
+        )
+
+        updates = telegram.Bot(conf.tg_bot_token).get_updates()
         for d in updates:
             chat = d.to_dict().get("channel_post", {}).get("chat", None)
             if chat and chat["type"] == "channel" and chat["title"] == channel_name:
@@ -152,39 +162,41 @@ If you have already done that, than hit 'Enter'.""",
             break
         else:
             click.echo(
-                f"Couldn't get the channel. Make sure you did everything right and try again."
+                "Couldn't get the channel. Make sure you did everything "
+                "right and try again."
             )
-
     if not tg_chat_id:
         return
 
+    channels_in_conf = [d["name"] for d in conf.channels]
     name = None
-    existing_channel_names = [d["name"] for d in conf.channels]
     for i in range(5):  # type: ignore
         name = click.prompt('Enter name, for example, "cute_dogs"')
-        if name in existing_channel_names:
-            click.echo("Channel with this name already exist. Try again.")
+
+        if name in channels_in_conf:
+            click.echo("Channel with this name already in 'config.json'. Try again.")
         elif len(re.findall(r"[^a-z0-9_]+", name)) > 0:
             click.echo("Can except only this symbols: a-z, 0-9, _. Try again.")
         else:
             break
-
     if not name:
         return
 
     with open(os.path.join(instance_path, "config.json"), "r+") as f:
-        config_content = json.load(f)
-        if not config_content.get("channels"):
-            config_content["channels"] = []
-        channelconf: ConfChannel = {
+        conf_content = json.load(f)
+        if "channels" not in conf_content:
+            conf_content["channels"] = []
+
+        channel_conf: ConfChannel = {
             "format_text": True,
             "name": name,
             "tg_chat_id": tg_chat_id,
             "vk_group_id": vk_group_id,
         }
-        config_content["channels"].append(channelconf)
+        conf_content["channels"].append(channel_conf)
+
         f.seek(0)
-        json.dump(config_content, f, sort_keys=True, indent=2)
+        json.dump(conf_content, f, sort_keys=True, indent=2)
 
 
 cli.add_command(add_channel_command)
