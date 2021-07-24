621ea6327e528a95613a0730ae58722e6763f95d
Lev <levwint@gmail.com>
2021-07-24 14:13:42 +0300

    feat: Add sentry integration for debugging; change version to 0.0.3

--- flow/__init__.py
+++ flow/__init__.py
@@ -3,7 +3,7 @@ import os
 
 from . import config
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 __description__ = "CLI for reposting from VK to Telegram"
 __url__ = "https://github.com/vrslev/flow"
@@ -19,3 +19,13 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.DEBUG,
 )
+
+if _sentry_dsn := os.environ.get("SENTRY_DSN"):
+    # pyright: reportMissingImports=false, reportUnknownVariableType=false
+    import sentry_sdk as _sentry_sdk
+    from sentry_sdk.integrations.logging import (
+        LoggingIntegration as _LoggingIntegration,
+    )
+
+    sentry_logging = _LoggingIntegration(level=logging.INFO, event_level=logging.ERROR)
+    _sentry_sdk.init(dsn=_sentry_dsn, integrations=[sentry_logging])
