3e989332e0cec2b486699dc3b25c1b2e7d0d6644
Lev Vereshchagin <mail@vrslev.com>
2022-01-05 18:48:46 +0300

    Copy directory instead of cloning the repo in preparing artifact script
    This helps for debugging

--- scripts/prepare_artifact.sh
+++ scripts/prepare_artifact.sh
@@ -6,8 +6,9 @@ TEMP_DIR=/tmp/flow
 ARCHIVE_NAME="$CUR_DIR/$(poetry version -s).zip"
 
 rm -rf $TEMP_DIR
-git clone . $TEMP_DIR
+cp -r . $TEMP_DIR
 cd $TEMP_DIR
+rm -r flow/__pycache__
 poetry export -o requirements.txt --without-hashes
 
 zip -r $ARCHIVE_NAME requirements.txt flow
